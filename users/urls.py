69e65fbca3cb8e082ab66bc6c000fc2e1f957114
Gabriel Sarmento <gabrielfs.bot@gmail.com>
2019-08-29 20:45:26 -0300

    Complete user crud

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from users.views import list_users
-from users.views import save_users
+from users.views import save_user
+from users.views import update_user
+from users.views import delete_user
 
 
 urlpatterns = [
     path('list/', list_users, name='list_users'),
-    path('new/', save_users, name='save_users')
+    path('new/', save_user, name='save_user'),
+    path('update/<int:id>/', update_user, name='update_user'),
+    path('delete/<int:id>/', delete_user, name='delete_user')
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from users.forms import UserForm
 from users.models import User
 
@@ -7,9 +7,25 @@ def list_users(request):
     users = User.objects.all()
     return render(request, 'base.html', {'users': users})
 
-def save_users(request):
+
+def save_user(request):
     form = UserForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('list_users')
     return render(request, 'user_form.html', {'form': form})
+
+
+def update_user(request, id):
+    user = get_object_or_404(User, pk=id)
+    form = UserForm(request.POST or None, instance=user)
+    if form.is_valid():
+        form.save()
+        return redirect('list_users')
+    return render(request, 'user_form.html', {'form': form})
+
+
+def delete_user(request, id):
+    user = get_object_or_404(User, pk=id)
+    user.delete()
+    return redirect('list_users')
